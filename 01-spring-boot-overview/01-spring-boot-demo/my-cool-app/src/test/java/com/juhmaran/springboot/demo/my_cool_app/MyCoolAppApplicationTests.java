1e5e965fb29d8495036f02a90e6db30b1a36146d
JulianeMaran32 <julianemaran@gmail.com>
2025-03-16 16:53:36 -0300

    Spring Boot Demo

---
+++ 01-spring-boot-overview/01-spring-boot-demo/my-cool-app/src/test/java/com/juhmaran/springboot/demo/my_cool_app/MyCoolAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juhmaran.springboot.demo.my_cool_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCoolAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
