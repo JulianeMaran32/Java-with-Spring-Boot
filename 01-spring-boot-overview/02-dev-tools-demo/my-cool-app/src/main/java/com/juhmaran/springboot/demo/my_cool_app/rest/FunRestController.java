eb21767e587d062a6adf839862bba4a5af526ad1
JulianeMaran32 <julianemaran@gmail.com>
2025-03-16 17:46:28 -0300

    adiciona readme

---
+++ 01-spring-boot-overview/02-dev-tools-demo/my-cool-app/src/main/java/com/juhmaran/springboot/demo/my_cool_app/rest/FunRestController.java
@@ -0,0 +1,16 @@
+package com.juhmaran.springboot.demo.my_cool_app.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // expose "/" that return "Hello World!"
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+}
