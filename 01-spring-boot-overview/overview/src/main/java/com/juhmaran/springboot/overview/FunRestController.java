9466170b1f994cb7ef18cea347af9026648324f8
JulianeMaran32 <julianemaran@gmail.com>
2025-03-30 18:15:35 -0300

    doc: atualiza readme

--- 01-spring-boot-overview/overview/src/main/java/com/juhmaran/springboot/overview/FunRestController.java
+++ 01-spring-boot-overview/overview/src/main/java/com/juhmaran/springboot/overview/FunRestController.java
@@ -1,4 +1,16 @@
 package com.juhmaran.springboot.overview;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FunRestController {
+
+    // expose "/" that return "Hello World"
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World";
+    }
+
 }
