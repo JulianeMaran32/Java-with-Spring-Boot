3a666fb1a65c79ef9ac743f6b3f4e2c65f07669a
JulianeMaran32 <julianemaran@gmail.com>
2025-04-03 21:32:30 -0300

    feat: adiciona dependecia spring security

--- 01-spring-boot-overview/overview/src/main/java/com/juhmaran/springboot/overview/rest/FunRestController.java
+++ 01-spring-boot-overview/overview/src/main/java/com/juhmaran/springboot/overview/rest/FunRestController.java
@@ -1,4 +1,4 @@
-package com.juhmaran.springboot.overview;
+package com.juhmaran.springboot.overview.rest;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
