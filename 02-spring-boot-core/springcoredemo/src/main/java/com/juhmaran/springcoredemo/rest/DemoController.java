7db8433995d6cf9520e8095f4d9855c4b828fb35
JulianeMaran32 <julianemaran@gmail.com>
2025-04-06 19:45:21 -0300

    Setter Injection

--- 02-spring-boot-core/springcoredemo/src/main/java/com/juhmaran/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-core/springcoredemo/src/main/java/com/juhmaran/springcoredemo/rest/DemoController.java
@@ -11,11 +11,16 @@ public class DemoController {
     // define a private field for the dependency
     private Coach myCoach;
 
-    // define a constructor for dependency injeciton
     @Autowired
-    public DemoController(Coach myCoach) {
+    public void setMyCoach(Coach myCoach) {
         this.myCoach = myCoach;
     }
+
+    // define a constructor for dependency injeciton
+//    @Autowired
+//    public DemoController(Coach myCoach) {
+//        this.myCoach = myCoach;
+//    }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
