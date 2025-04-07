59c5e12dd375c60e81cc28bf38bfaf4224a1fd4a
JulianeMaran32 <julianemaran@gmail.com>
2025-04-06 22:41:37 -0300

    Primary

--- 02-spring-boot-core/springcoredemo/src/main/java/com/juhmaran/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-core/springcoredemo/src/main/java/com/juhmaran/springcoredemo/rest/DemoController.java
@@ -2,7 +2,6 @@ package com.juhmaran.springcoredemo.rest;
 
 import com.juhmaran.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +10,8 @@ public class DemoController {
 
     private Coach myCoach;
 
-    // Specify the bean id: baseaballCoach
-    // Same name as class, first character lower-case
     @Autowired
-    public DemoController(@Qualifier(value = "baseballCoach") Coach myCoach) {
+    public DemoController(Coach myCoach) {
         this.myCoach = myCoach;
     }
 
