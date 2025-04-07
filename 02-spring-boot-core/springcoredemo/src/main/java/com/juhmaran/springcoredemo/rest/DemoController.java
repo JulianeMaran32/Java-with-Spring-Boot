50d7a185a1d89a819512f59e40fa9c3b95ae2e9b
JulianeMaran32 <julianemaran@gmail.com>
2025-04-06 22:04:35 -0300

    Qualifiers

--- 02-spring-boot-core/springcoredemo/src/main/java/com/juhmaran/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-core/springcoredemo/src/main/java/com/juhmaran/springcoredemo/rest/DemoController.java
@@ -2,25 +2,21 @@ package com.juhmaran.springcoredemo.rest;
 
 import com.juhmaran.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
-    // define a private field for the dependency
     private Coach myCoach;
 
+    // Specify the bean id: baseaballCoach
+    // Same name as class, first character lower-case
     @Autowired
-    public void setMyCoach(Coach myCoach) {
+    public DemoController(@Qualifier(value = "baseballCoach") Coach myCoach) {
         this.myCoach = myCoach;
     }
-
-    // define a constructor for dependency injeciton
-//    @Autowired
-//    public DemoController(Coach myCoach) {
-//        this.myCoach = myCoach;
-//    }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout() {
